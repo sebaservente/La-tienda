340218ff2970e5d52ddcdc5d4e95e83151126de5
sersebas08 <85594721+sebaservente@users.noreply.github.com>
2021-07-16 10:49:01 -0300

    ordenamos el codigo para iniciar la recuperacion por medio de token

--- servente-sebastian_tp/secciones/registro.php
+++ servente-sebastian_tp/secciones/registro.php
@@ -15,7 +15,7 @@ $oldData = sessionValueGetFlash('old_data', []);
             <legend>Datos de identificación</legend>
             <div class="form-file">
                 <label for="email">Email</label>
-                <input  type="email" id="email" name="email" class="form-control shadow" autocomplete="off" placeholder="ser@ser.com.ar"
+                <input  type="email" id="email" name="email" class="form-control shadow" autocomplete="off" placeholder="ejemplo@ejemplo.com.ar"
                     <?php if(isset($errores['email'])) echo 'aria-describedby="error-email"';?>
                         value="<?= $oldData['email'] ?? '';?>">
                 <?php
@@ -28,7 +28,7 @@ $oldData = sessionValueGetFlash('old_data', []);
                 <label for="password">Password</label>
                 <!--<input type="password" id="password" name="password" class="form-control">-->
 
-                <input  type="password" id="password" name="password" class="form-control shadow" autocomplete="off" placeholder="******"
+                <input  type="password" id="password" name="password" class="form-control shadow" autocomplete="off" placeholder="minimo 6 caracteres"
                     <?php if(isset($errores['password'])) echo 'aria-describedby="error-password"';?>
                         value="<?= $oldData['password'] ?? '';?>">
                 <?php
