eed769629ecf050fd5e62fc401d735773a5c9959
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2021-07-24 13:14:29 -0300

    editando el perfil del usuario

--- servente-sebastian_tp/secciones/perfil.php
+++ servente-sebastian_tp/secciones/perfil.php
@@ -2,15 +2,29 @@
 require 'data/bootstrap.php';
 
 $usuario = usuarioTodos($db);
-echo "<pre>";
+/*echo "<pre>";
 print_r($usuario);
-echo "</pre>";
+echo "</pre>";*/
+if(!authEstaAutenticado()) {
+    /*    $_SESSION['seccion_preten'] = $seccion;*/
+    $_SESSION['successInfo'] = "Necesitas estar autenticado para realizar esta accion";
+    header('Location: ../index.php?s=login');
+    exit;
+}
 ?>
 <section class="container">
     <h2>Perfil de Usuarios</h2>
-    <p>Email: <?= authObtenerUsuario()['email'];?></p>
-    <p>Nombre: <?= authObtenerUsuario()['nombre'] ?></p>
-    <p>Apellido: <?= authObtenerUsuario()['apellido'] ?></p>
-    <p>ID:<?= authObtenerUsuario()['id_usuario'] ?></p>
-    <a href="index.php?s=editar-usuario&id=<?= authObtenerUsuario()['id_usuario'];?>">Editar usuario</a>
+    <?php
+    foreach ($usuario as $usuarios):
+    ?>
+    <div>
+        <p>Email: <?=$usuarios['email'];?></p>
+        <p>Nombre: <?= $usuarios['nombre']; ?></p>
+        <p>Apellido: <?=$usuarios['apellido']; ?></p>
+        <p>ID:<?=$usuarios['id_usuario']; ?></p>
+        <a href="index.php?s=editar-usuario&id=<?= authObtenerUsuario()['id_usuario'];?>">Editar usuario</a>
+    </div>
+    <?php
+    endforeach;
+    ?>
 </section>
